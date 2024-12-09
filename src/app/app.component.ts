9bb0d2ebf9ae4591250b973498bba52e85099dbd
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-12-09 18:22:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavAuthComponent } from './components/nav-auth/nav-auth.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { MainComponent } from './layouts/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ecommerce';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './layouts/main/main.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, title: 'Login' },
+      { path: 'register', component: RegisterComponent, title: 'Register' },
+    ],
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent, title: 'Home' },
+      { path: 'products', component: ProductsComponent, title: 'Products' },
+      { path: 'cart', component: CartComponent, title: 'Cart' },
+      { path: 'brands', component: BrandsComponent, title: 'Brands' },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+        title: 'Categories',
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
