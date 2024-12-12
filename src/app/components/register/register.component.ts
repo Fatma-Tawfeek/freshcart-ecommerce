9f7a45a9bc3c5c345004ef9760af2203a7de5819
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-12-12 02:11:32 +0200

    create register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
 
+        Validators.pattern(/^\w{6,}$/),
+      ]),
+      rePassword: new FormControl(null),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^01[0125][0-9]{8}$/),
+      ]),
+    },
+    this.confirmPassword
+  );
+
+  register() {
+    console.log(this.registerForm.value);
+  }
+
+  // custom validator
+  confirmPassword(g: AbstractControl) {
+    if (g.get('password')?.value !== g.get('rePassword')?.value) {
+      return { misMatch: true };
+    }
+    return null;
+  }
 }
