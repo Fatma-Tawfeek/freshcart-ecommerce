61ebadb37bb2ddf1ec211a964c0b2f4bfef75959
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-01-11 04:53:41 +0200

    add brands component

--- src/app/components/brands/brands.component.ts
+++ src/app/components/brands/brands.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { BrandsService } from '../../core/services/brands.service';
+import { IBrand } from '../../core/interfaces/brands';
+import { Subscription } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './brands.component.html',
-  styleUrl: './brands.component.css'
+  styleUrl: './brands.component.css',
 })
 export class BrandsComponent {
+  brandsData!: IBrand[];
+  brandSub!: Subscription;
+  currentPage!: number;
+  numOfPages!: number;
 
+  constructor(private __BrandsService: BrandsService) {}
+
+  getPageByNumber(page: number) {
+    this.brandSub = this.__BrandsService.getAllBrands(page).subscribe({
+      next: (res) => {
+        this.currentPage = res.metadata.currentPage;
+        this.numOfPages = res.metadata.numberOfPages;
+        this.brandsData = res.data;
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.numOfPages }, (_, i) => i + 1);
+  }
+
+  ngOnInit(): void {
+    this.brandSub = this.__BrandsService.getAllBrands().subscribe({
+      next: (res) => {
+        this.brandsData = res.data;
+        this.currentPage = res.metadata.currentPage;
+        this.numOfPages = res.metadata.numberOfPages;
+      },
+    });
+  }
 }
