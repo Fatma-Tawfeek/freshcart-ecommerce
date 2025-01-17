d22f5fec911cd380a10ae3b0469b8f15f7fee709
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-01-17 07:56:39 +0200

    Add wishlist feature to product details page

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -5,6 +5,8 @@ import { IProduct } from '../../core/interfaces/product';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { CartService } from '../../core/services/cart.service';
 import { ToastrService } from 'ngx-toastr';
+import { WhishlistService } from '../../core/services/whishlist.service';
+import { IWishlist } from '../../core/interfaces/whishlist';
 
 @Component({
   selector: 'app-product-details',
@@ -44,20 +46,45 @@ export class ProductDetailsComponent implements OnInit {
   private readonly __ActivatedRoute = inject(ActivatedRoute);
   productId!: string | null;
   productDetails: IProduct | null = null;
+  whishlistData!: IWishlist[];
+  whishlistIds!: string[];
 
   constructor(
     private __ProductService: ProductsService,
     private __CarteService: CartService,
-    private __ToastrService: ToastrService
+    private __ToastrService: ToastrService,
+    private __WhishlistService: WhishlistService
   ) {}
 
   addCartItem(p_id: string) {
     this.__CarteService.addItemToCart(p_id).subscribe({
       next: (res) => {
-        console.log(res);
+        this.__CarteService.cartCount.next(res.numOfCartItems);
         this.__ToastrService.success(res.message, 'Success');
       },
     });
+  }
+
+  addToWishlist(p_id: string) {
+    this.__WhishlistService.addItemToWishlist(p_id).subscribe({
+      next: (res) => {
+        this.__ToastrService.success(res.message, 'Success');
+        this.whishlistIds = res.data;
+      },
+    });
+  }
+
+  removeFromWishlist(p_id: string) {
+    this.__WhishlistService.removeItemFromWishlist(p_id).subscribe({
+      next: (res) => {
+        this.__ToastrService.success(res.message, 'Success');
+        this.whishlistIds = res.data;
+      },
+    });
+  }
+
+  isProductInWishlist(productId: string): boolean {
+    return this.whishlistIds.includes(productId);
   }
   ngOnInit(): void {
     this.__ActivatedRoute.paramMap.subscribe({
@@ -69,6 +96,13 @@ export class ProductDetailsComponent implements OnInit {
     this.__ProductService.getProductDetails(this.productId).subscribe({
       next: (res) => {
         this.productDetails = res.data;
+      },
+    });
+
+    this.__WhishlistService.getWishlist().subscribe({
+      next: (res) => {
+        this.whishlistData = res.data;
+        this.whishlistIds = this.whishlistData.map((item) => item._id);
       },
     });
   }
